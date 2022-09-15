d5a12d31d47ec35dceae96ce98af52a5e7a59611
devincrack <santoshjnvropar@gmail.com>
2022-09-15 13:55:54 +0000

    hogya

--- Meta/settings.py
+++ Meta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q=!dpjqmmcd*jxigbjdxkt#ve*@vncw*&d2j^z%*5e@d96z)8u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
