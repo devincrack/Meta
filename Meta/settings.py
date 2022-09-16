a2f8b926850c55a057373480c7b18f01642b2e8d
devincrack <santoshjnvropar@gmail.com>
2022-09-16 02:55:18 +0000

    hogya

--- Meta/settings.py
+++ Meta/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-#STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 # Default primary key field type
 MEDIA_URL = "/media/"
 #MEDIA_ROOT = BASE_DIR / "media"
