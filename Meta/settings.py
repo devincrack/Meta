07426b9e29e114d925620296e35c7e376d6cbd56
devincrack <santoshjnvropar@gmail.com>
2022-09-16 01:04:52 +0000

    hogya

--- Meta/settings.py
+++ Meta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q=!dpjqmmcd*jxigbjdxkt#ve*@vncw*&d2j^z%*5e@d96z)8u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["metagram-z.herokuapp.com"]
 
 
 # Application definition
